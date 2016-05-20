7d27f316023f718b4dce70e9f195569a99bf9bcb
leonardoalbuquerque <leonardo.lopes.albuquerque@gmail.com>
2016-05-20 10:25:09 -0300

    commit inicial

--- Prova2pthreads/main.c
+++ Prova2pthreads/main.c
@@ -1,15 +1,36 @@
-//
-//  main.c
-//  Prova2pthreads
-//
-//  Created by leonardo albuquerque on 20/05/16.
-//  Copyright © 2016 leonardo albuquerque. All rights reserved.
-//
-
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#define NUM_THREADS     5
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+struct d{
+    int a;
+    float b;
+} dados[NUM_THREADS];
+
+void *PrintHello(struct d *threadid)
+{
+    printf("%d: Iniciou!\n", threadid->a);
+    int i = 0;
+    while(i < 100000000){
+        i++;
+    }
+    printf("%d: Finalizou!\n", threadid->a);
+    pthread_exit(NULL);
+}
+int main(int argc, char *argv[])
+{
+    pthread_t threads[NUM_THREADS];
+    int rc, t;
+    for (t=0; t < NUM_THREADS; t++) {
+        printf("Criando o thread %d\n", t);
+        dados[t].a=t;
+        dados[t].b=t*3.14;
+        rc = pthread_create( &threads[t], NULL, PrintHello, &dados[t]);
+        if (rc) {
+            printf("Erro ao retornar do thread: %d\n", rc);
+            exit(-1);
+        }
+    }
+    pthread_exit(NULL);
 }
